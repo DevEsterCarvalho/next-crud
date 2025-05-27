c0aae7adcf5ebffc0d217da4c63dcd9e85f48520
DevEsterCarvalho <estercarvalho205@gmail.com>
2025-05-27 16:46:48 -0300

    criando componente tabela

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,22 @@
 import Layout from "../components/Layout";
+import Tabela from "../components/Tabela";
+import Cliente from "../core/Cliente";
 
 export default function Home() {
+	const clientes = [
+		new Cliente("Ana", 34, "1"),
+		new Cliente("Bia", 45, "2"),
+		new Cliente("Carlos", 49, "3"),
+		new Cliente("Pedro", 50, "4"),
+	];
+
+	function clienteSelecionado(cliente: Cliente) {
+		console.log(cliente.nome);
+	}
+
+	function clienteExcluido(cliente: Cliente) {
+		console.log(`Excluir... ${cliente.nome}`);
+	}
 	return (
 		<div
 			className={`
@@ -10,7 +26,11 @@ export default function Home() {
 		  `}
 		>
 			<Layout titulo="Cadastro Simples">
-				<span>Conteudo</span>
+				<Tabela
+					clientes={clientes}
+					clienteSelecionado={clienteSelecionado}
+					clienteExcluido={clienteExcluido}
+				></Tabela>
 			</Layout>
 		</div>
 	);
