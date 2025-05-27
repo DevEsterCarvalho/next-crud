0ff64636c039e6bd2abd94f6e90c09af3148e2eb
DevEsterCarvalho <estercarvalho205@gmail.com>
2025-05-27 16:58:54 -0300

    ajustando componente titulo

--- src/components/Titulo.tsx
+++ src/components/Titulo.tsx
@@ -1,7 +1,13 @@
-export default function Titulo(props) {
+import { ReactNode } from "react";
+
+interface TituloProps {
+	children: ReactNode;
+}
+
+export default function Titulo({ children }: TituloProps) {
 	return (
 		<div className="flex flex-col justify-center">
-			<h1 className="px-5 py-2 text-2xl">{props.children}</h1>
+			<h1 className="px-5 py-2 text-2xl">{children}</h1>
 			<hr className="border-2 border-purple-500" />
 		</div>
 	);
