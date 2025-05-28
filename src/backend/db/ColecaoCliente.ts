5000072f10c6eef05af9ab42873b5562c8921629
DevEsterCarvalho <estercarvalho205@gmail.com>
2025-05-28 15:58:51 -0300

    editando colecao cliente

--- src/backend/db/ColecaoCliente.ts
+++ src/backend/db/ColecaoCliente.ts
@@ -3,14 +3,14 @@ import Cliente from "../../core/Cliente";
 
 export default class ColecaoCliente implements ClienteRepositorio {
 	async salvar(cliente: Cliente): Promise<Cliente> {
-		return null;
+		return cliente; // retorna o mesmo cliente que recebeu por enquanto
 	}
 
 	async excluir(cliente: Cliente): Promise<void> {
-		return null;
+		return;
 	}
 
-	obterTodos(): Promise<Cliente[]> {
-		return null;
+	async obterTodos(): Promise<Cliente[]> {
+		return []; //array vazio enquanto esta e desenvolvimento
 	}
 }
